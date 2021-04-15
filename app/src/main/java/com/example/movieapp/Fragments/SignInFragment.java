9d09998bd987c77302f46c66e6feb1beb1788db2
sayrsalad <sayrsalad@users.noreply.github.com>
2021-04-15 21:55:36 +0800

    Sign In and up layout

---
+++ app/src/main/java/com/example/movieapp/Fragments/SignInFragment.java
@@ -0,0 +1,25 @@
+package com.example.movieapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.movieapp.R;
+
+public class SignInFragment extends Fragment {
+    private View view;
+
+    public SignInFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.layout_sign_in, container, false);
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/movieapp/Fragments/SignUpFragment.java
@@ -0,0 +1,25 @@
+package com.example.movieapp.Fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.movieapp.R;
+
+public class SignUpFragment extends Fragment {
+    private View view;
+
+    public SignUpFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.layout_sign_up, container, false);
+        return view;
+    }
+}
