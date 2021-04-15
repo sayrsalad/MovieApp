391bacd5cea681aa6a1ef2ad21db8bc0979e2205
sayrsalad <sayrsalad@users.noreply.github.com>
2021-04-16 01:07:20 +0800

    Sign In and up layout 2

--- app/src/main/java/com/example/movieapp/Fragments/SignInFragment.java
+++ app/src/main/java/com/example/movieapp/Fragments/SignInFragment.java
@@ -1,18 +1,28 @@
 package com.example.movieapp.Fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.movieapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignInFragment extends Fragment {
     private View view;
+    private TextInputLayout layoutEmail, layoutPassword;
+    private TextInputEditText txtEmail, txtPassword;
+    private TextView txtSignUp;
+    private Button btnSignIn;
 
     public SignInFragment() {}
 
@@ -20,6 +30,79 @@ public class SignInFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.layout_sign_in, container, false);
+        init();
         return view;
+    }
+
+    private void init() {
+        layoutPassword = view.findViewById(R.id.txtLayoutPasswordSignIn);
+        layoutEmail = view.findViewById(R.id.txtLayoutEmailSignIn);
+        txtPassword = view.findViewById(R.id.txtPasswordSignIn);
+        txtEmail = view.findViewById(R.id.txtEmailSignIn);
+        txtSignUp = view.findViewById(R.id.txtSignUp);
+        btnSignIn = view.findViewById(R.id.btnSignIn);
+
+        txtSignUp.setOnClickListener(v->{
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer, new SignUpFragment()).commit();
+        });
+
+        btnSignIn.setOnClickListener(v->{
+            if (validate()) {
+
+            }
+        });
+
+        txtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtEmail.getText().toString().isEmpty()) {
+                    layoutEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtPassword.getText().toString().length()>7) {
+                    layoutPassword.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private boolean validate() {
+        if (txtEmail.getText().toString().isEmpty()) {
+            layoutEmail.setErrorEnabled(true);
+            layoutEmail.setError("Enter a valid e-mail");
+            return false;
+        }
+
+        if (txtPassword.getText().toString().length()<8) {
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Required at least 8 characters");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/example/movieapp/Fragments/SignUpFragment.java
+++ app/src/main/java/com/example/movieapp/Fragments/SignUpFragment.java
@@ -1,18 +1,28 @@
 package com.example.movieapp.Fragments;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.movieapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class SignUpFragment extends Fragment {
     private View view;
+    private TextInputLayout layoutEmail, layoutPassword, layoutConfirm;
+    private TextInputEditText txtEmail, txtPassword, txtConfirm;
+    private TextView txtSignIn;
+    private Button btnSignUp;
 
     public SignUpFragment() {}
 
@@ -20,6 +30,107 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.layout_sign_up, container, false);
+        init();
         return view;
+    }
+
+    private void init() {
+        layoutPassword = view.findViewById(R.id.txtLayoutPasswordSignUp);
+        layoutEmail = view.findViewById(R.id.txtLayoutEmailSignUp);
+        layoutConfirm = view.findViewById(R.id.txtLayoutConfirmSignUp);
+        txtPassword = view.findViewById(R.id.txtPasswordSignUp);
+        txtEmail = view.findViewById(R.id.txtEmailSignUp);
+        txtConfirm = view.findViewById(R.id.txtConfirmSignUp);
+        txtSignIn = view.findViewById(R.id.txtSignIn);
+        btnSignUp = view.findViewById(R.id.btnSignUp);
+
+        txtSignIn.setOnClickListener(v->{
+            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.frameAuthContainer, new SignInFragment()).commit();
+        });
+
+        btnSignUp.setOnClickListener(v->{
+            if (validate()) {
+
+            }
+        });
+
+        txtEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtEmail.getText().toString().isEmpty()) {
+                    layoutEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        txtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtPassword.getText().toString().length()>7) {
+                    layoutPassword.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        txtConfirm.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (txtConfirm.getText().toString().equals(txtPassword.getText().toString())) {
+                    layoutConfirm.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    private boolean validate() {
+        if (txtEmail.getText().toString().isEmpty()) {
+            layoutEmail.setErrorEnabled(true);
+            layoutEmail.setError("Enter a valid e-mail");
+            return false;
+        }
+
+        if (txtPassword.getText().toString().length()<8) {
+            layoutPassword.setErrorEnabled(true);
+            layoutPassword.setError("Required at least 8 characters");
+            return false;
+        }
+
+        if (!txtConfirm.getText().toString().equals(txtPassword.getText().toString())) {
+            layoutConfirm.setErrorEnabled(true);
+            layoutConfirm.setError("Password does not match");
+            return false;
+        }
+
+        return true;
     }
 }
